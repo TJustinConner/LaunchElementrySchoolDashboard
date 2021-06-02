e0670ee1a768ca65ffb3ce82107389b506f04a30
akd2yf <akd2yf@virginia.edu>
2021-06-02 08:35:26 -0400

    minor changes

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -2,8 +2,8 @@ export default function Teacher( { teacher } ) {
     return(
         <div>
             <h2 align='center'>{teacher.first} {teacher.last}</h2>
-            <div>Phone Number: {teacher.contact[0]}</div>
-            <div>Email: {teacher.contact[1]}</div>
+            <div>&nbsp;Phone Number: {teacher.contact[0]}</div>
+            <div>&nbsp;Email: {teacher.contact[1]}</div>
         </div>
     );
 }
